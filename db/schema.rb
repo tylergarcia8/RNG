16edd2fe22c06d42a4a5f11d6e576b3a1b3f10fa
Tyler Garcia <t-garcia15@hotmail.com>
2016-04-24 17:37:38 -0400

    categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424160343) do
+ActiveRecord::Schema.define(version: 20160424194211) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "category_name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
     t.text     "description"
-    t.integer  "owner_id" # to be removed
-    t.integer  "renter_id" 
+    t.integer  "owner_id"
+    t.integer  "renter_id"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
