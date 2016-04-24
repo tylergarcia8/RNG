f22d3d313a0e83a46775c8bc850a0b35100a78a0
Gabo Martinez <gabmart02@gmail.com>
2016-04-24 13:15:21 -0400

    Validation that product has name, price, description, and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, :description, :category_id, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 0}
 end
