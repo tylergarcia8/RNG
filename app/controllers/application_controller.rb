c918edfecf861913d3d1408d97147306c547402f
Michael Busta <busta2356@gmail.com>
2016-04-24 18:46:39 -0400

    Fixing Sign Up error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
 helper_method :current_user
 
-  def authorize
+  def authenticate
     redirect_to '/login' unless current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, except: :create
+  before_action :authenticate, except: [:create, :new]
   # GET /users
   def index
     @users = User.all
