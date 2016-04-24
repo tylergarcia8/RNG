5144cedb77d45ccad96378dd30ff8af048beb7ba
Gabo Martinez <gabmart02@gmail.com>
2016-04-24 19:54:10 -0400

    adding category reference to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424194211) do
+ActiveRecord::Schema.define(version: 20160424235311) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
@@ -26,12 +26,13 @@ ActiveRecord::Schema.define(version: 20160424194211) do
     t.text     "description"
     t.integer  "owner_id"
     t.integer  "renter_id"
-    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
   add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "profiles", force: :cascade do |t|
